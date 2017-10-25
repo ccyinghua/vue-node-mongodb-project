f901b49f0da60871dbe7b237380c428720d3f8f7
wu <2697312908@qq.com>
2017-10-25 23:40:21 +0800

    add
    nodeJs基础添加-server文件夹

---
+++ server/User.js
@@ -0,0 +1,15 @@
+// 两种导出Demo.js的结果一样
+
+// module.exports = {
+//     userName:"Jack",
+//     sayHello:function(){
+//         return 'Hello';
+//     }
+// }
+
+exports.userName = "Make";
+exports.sayHello = function(){
+    return 'Hello';
+}
+
+
